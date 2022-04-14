521706f309f04fad1274ab22157f998d1e6d6cca
Samuel Garratt <samuel.garratt@integrationqa.com>
2022-04-14 21:42:16 +1200

    Initial commit

---
+++ lib/helm_upgrade_logs.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "helm_upgrade_logs/version"
+
+module HelmUpgradeLogs
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/helm_upgrade_logs_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe HelmUpgradeLogs do
+  it "has a version number" do
+    expect(HelmUpgradeLogs::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
