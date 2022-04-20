2032d5cf20a449d4511157e60d67a9a8d828dcf1
Samuel Garratt <samuel.garratt@integrationqa.com>
2022-04-21 08:47:36 +1200

    Fix rubocop errors but remove it from blocking CI

--- lib/helm_upgrade_logs.rb
+++ lib/helm_upgrade_logs.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-require 'open3'
+require "open3"
 require_relative "helm_upgrade_logs/version"
 
-ENV['helm_upgrade_logs_log_start'] ||= '90'
-ENV['helm_upgrade_logs_pod_start'] ||= '35'
+ENV["helm_upgrade_logs_log_start"] ||= "90"
+ENV["helm_upgrade_logs_pod_start"] ||= "35"
 
 # Approach not ideal as it will for all containers to be ready and want to stream logs before that
 def wait_for_container_ready
@@ -15,38 +15,39 @@ end
 # Wait for very first pods with logs to be present
 # Not ideal due to https://github.com/kubernetes/kubernetes/issues/28746
 def wait_for_pod_to_log
-  ENV['helm_upgrade_logs_log_start'].to_i.times do |i|
-    return if Process.waitpid(@helm_pid, Process::WNOHANG) != nil
+  ENV["helm_upgrade_logs_log_start"].to_i.times do |i|
+    break unless Process.waitpid(@helm_pid, Process::WNOHANG).nil?
+
     sleep 1
-    stdout, stderr, _ = Open3.capture3(add_ns("kubectl logs -lapp.kubernetes.io/instance=#{$release_name}"))
+    stdout, stderr, = Open3.capture3(add_ns("kubectl logs -lapp.kubernetes.io/instance=#{@release_name}"))
     if stderr.empty? && !stdout.strip.empty?
-      puts '[INFO] Pods with logs found'
+      puts "[INFO] Pods with logs found"
       break
-    else
-      puts "[INFO] Waiting for pod logs: #{stderr}" if i % 3 == 0
+    elsif (i % 3).zero?
+      puts "[INFO] Waiting for pod logs: #{stderr}"
     end
   end
 end
 
 # Wait for logs from a specific pod
 def wait_for_specific_pod_to_log(pod_name)
-  ENV['helm_upgrade_logs_pod_start'].to_i.times do |i|
+  ENV["helm_upgrade_logs_pod_start"].to_i.times do |i|
     sleep 1
-    stdout, stderr, _ = Open3.capture3(add_ns("kubectl logs #{pod_name}"))
+    stdout, stderr, = Open3.capture3(add_ns("kubectl logs #{pod_name}"))
     if stderr.empty? && !stdout.strip.empty?
       puts "[INFO] Pod #{pod_name} with logs found"
       break
-    else
-      puts "[INFO] Waiting for pod #{pod_name} logs: #{stderr}" if i % 2 == 0
+    elsif i.even?
+      puts "[INFO] Waiting for pod #{pod_name} logs: #{stderr}"
     end
   end
 end
 
 # Get pods
-def get_pods
-  stdout, stderr, _ = Open3.capture3(add_ns("kubectl get pods -lapp.kubernetes.io/instance=#{$release_name} -o name"))
+def read_pods
+  stdout, stderr, = Open3.capture3(add_ns("kubectl get pods -lapp.kubernetes.io/instance=#{@release_name} -o name"))
   if stderr.empty?
-    stdout.lines.collect { |pod| pod.strip }
+    stdout.lines.collect(&:strip)
   else
     []
   end
@@ -62,7 +63,7 @@ end
 
 # Add namespace to kube query
 def add_ns(kube_query)
-  kube_query += " -n #{$namespace}" if $namespace
+  kube_query += " -n #{@namespace}" if @namespace
   kube_query
 end
 
--- spec/helm_upgrade_logs_spec.rb
+++ spec/helm_upgrade_logs_spec.rb
@@ -8,6 +8,6 @@ RSpec.describe HelmUpgradeLogs do
   it "can extract namespace from args" do
     argv = %w[helm upgrade --install nginx -n dev]
     namespace = namespace_from_args(argv)
-    expect(namespace).to be 'dev'
+    expect(namespace).to be "dev"
   end
 end
